2c5fdc760c3911f6907ad149bbb112e39a1fab87
Sermob112 <motherboard879@gmail.com>
2023-07-31 13:09:03 +0300

    Сделана полность страница вход с заполнением полей и подсветкой кнопки вход

---
+++ MAINPAGES/scripts/authorizationScript.js
@@ -0,0 +1,41 @@
+// Получаем ссылки на поля логина и пароля
+const loginField = document.querySelector(".login-field");
+const passwordField = document.querySelector(".password-field");
+
+// Значения для заполнения полей
+const loginValue = "login";
+const passwordValue = "password";
+
+// Функция для имитации анимации печатания
+function simulateTyping(element, text, speed = 100) {
+  let index = 0;
+  function typeNextCharacter() {
+    if (index < text.length) {
+      element.value += text[index];
+      index++;
+      setTimeout(typeNextCharacter, speed);
+    }
+  }
+  typeNextCharacter();
+}
+
+// Запускаем анимацию при загрузке страницы
+window.onload = function () {
+  // Запускаем анимацию для поля логина
+  simulateTyping(loginField, loginValue);
+
+  // Запускаем анимацию для поля пароля после небольшой паузы
+  setTimeout(function () {
+    simulateTyping(passwordField, passwordValue);
+  }, 1500); // Измените эту паузу (в миллисекундах) в зависимости от вашего желания
+};
+
+const loginButton = document.querySelector(".login-button");
+
+// Функция для добавления класса подсветки
+function highlightButton() {
+  loginButton.classList.add("login-button-highlight");
+}
+
+// Запускаем функцию с задержкой в 3 секунды после загрузки страницы
+setTimeout(highlightButton, 3000);
